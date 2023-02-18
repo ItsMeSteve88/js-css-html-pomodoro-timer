7a4e14c33195632b5d9b4345fbb2358f62c9a870
Stephen James <78153905+ItsMeSteve88@users.noreply.github.com>
2023-02-18 17:23:22 +0000

    add tasks

--- js/app.js
+++ js/app.js
@@ -1,22 +1,78 @@
 const taskContainer = document.querySelector('.task-container')
+const submitButton = document.querySelector('.submit-button')
+
+
+let tasks = [
+    {
+        name: 'Practice CSS Animations',
+        priority: 0,
+    },
+    {
+        name: 'Dev community work',
+        priority: 2,
+    },
+    {
+        name: 'Algorithm studies',
+        priority: 1,
+    }
+]
+
+// sort by priority
+
+const descendingTasks = tasks.sort((taskA, taskB) => taskA.priority - taskB.priority)
+
 
 function render()
 {
-    const taskBlock = document.createElement('div')
-    const deleteElement = document.createElement('p')
-    const title = document.createElement('p')
-    const controller = document.createElement('button')
+    descendingTasks.forEach(task =>
+    {
+        const taskBlock = document.createElement('div')
+        const deleteElement = document.createElement('p')
+        const title = document.createElement('p')
+        const controller = document.createElement('button')
+    
+        taskBlock.classList.add('task-block')
+        deleteElement.classList.add('delete-icon')
+        controller.classList.add('controller-button')
+    
+        deleteElement.textContent = 'X'
+        title.textContent = task.name
+        controller.textContent = 'Start'
 
-    taskBlock.classList.add('task-block')
-    deleteElement.classList.add('delete-icon')
-    controller.classList.add('controller-button')
+        deleteElement.addEventListener('click', deleteTask)
+    
+        taskBlock.append(deleteElement, title, controller)
+        taskContainer.append(taskBlock)
+    })
 
-    deleteElement.textContent = 'X'
-    title.textContent = 'Task title'
-    controller.textContent = 'Start'
-
-    taskBlock.append(deleteElement, title, controller)
-    taskContainer.append(taskBlock)
 }
 
 render()
+
+function deleteTask(e)
+{
+    const task = e.target.parentNode
+    task.remove()
+    // console.log(tasks) toDo: remove from array
+}
+
+function addTask()
+{
+    // select input element (add task)
+    const inputElement = document.querySelector('input')
+    // get value from input element
+    const value = inputElement.value
+    console.log(value)
+    if (value)
+    {
+        taskContainer.innerHTML = ''
+        tasks.push({
+            name: value,
+            priority: tasks.length
+        })
+        render()
+    }
+}
+
+// add event listener to submit button to add the task
+submitButton.addEventListener('click', addTask)
